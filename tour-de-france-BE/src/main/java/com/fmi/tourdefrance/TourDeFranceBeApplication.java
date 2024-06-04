e8934ba2740304d77a965cdbfe4f508dbdc371a9
StoyanTinchev <st8tinchev@gmail.com>
2024-06-04 22:39:12 +0300

    Add initial BE setup

---
+++ tour-de-france-BE/src/main/java/com/fmi/tourdefrance/TourDeFranceBeApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.tourdefrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourDeFranceBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourDeFranceBeApplication.class, args);
+    }
+
+}
---
+++ tour-de-france-BE/src/test/java/com/fmi/tourdefrance/TourDeFranceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.tourdefrance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourDeFranceBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
